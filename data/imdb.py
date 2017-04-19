ab626b25b9b95aed44240e01bbd50d3fb0c22c55
DantongJi <jidantong@gmail.com>
2017-04-18 23:42:33 -0400

    validation set

--- data/imdb.py
+++ data/imdb.py
@@ -96,8 +96,8 @@ def get_dataset_file(dataset, default_dataset, origin):
     return dataset
 
 
-def load_data(path="imdb.pkl", n_words=100000, valid_portion=0.1, maxlen=None,
-              sort_by_len=True):
+def load_data(test_path="imdb_test.pkl", train_path = 'imdb_train.pkl', n_words=10000, valid_portion=0.2, maxlen=None,
+              sort_by_len=False):
     '''Loads the dataset
 
     :type path: String
@@ -123,18 +123,22 @@ def load_data(path="imdb.pkl", n_words=100000, valid_portion=0.1, maxlen=None,
     #############
 
     # Load the dataset
-    path = get_dataset_file(
-        path, "imdb.pkl",
-        "http://www.iro.umontreal.ca/~lisa/deep/data/imdb.pkl")
+    # path = get_dataset_file(
+    #     path, "imdb.pkl",
+    #     "http://www.iro.umontreal.ca/~lisa/deep/data/imdb.pkl")
 
-    if path.endswith(".gz"):
-        f = gzip.open(path, 'rb')
-    else:
-        f = open(path, 'rb')
+    # if path.endswith(".gz"):
+    #     f = gzip.open(path, 'rb')
+    # else:
+    #     f = open(path, 'rb')
 
-    train_set = pickle.load(f)
-    test_set = pickle.load(f)
-    f.close()
+    f_train = open(train_path, 'rb')
+    f_test = open(test_path, 'rb')
+
+    train_set = pickle.load(f_train)
+    test_set = pickle.load(f_test)
+    f_train.close()
+    f_test.close()
     if maxlen:
         new_train_set_x = []
         new_train_set_y = []
@@ -146,24 +150,26 @@ def load_data(path="imdb.pkl", n_words=100000, valid_portion=0.1, maxlen=None,
         del new_train_set_x, new_train_set_y
 
     # split training set into validation set
-    train_set_x, train_set_y = train_set
+    train_set_x, train_set_y, train_set_score = train_set
     n_samples = len(train_set_x)
     sidx = numpy.random.permutation(n_samples)
     n_train = int(numpy.round(n_samples * (1. - valid_portion)))
     valid_set_x = [train_set_x[s] for s in sidx[n_train:]]
     valid_set_y = [train_set_y[s] for s in sidx[n_train:]]
+    valid_set_score = [train_set_score[s] for s in sidx[n_train:]]
     train_set_x = [train_set_x[s] for s in sidx[:n_train]]
     train_set_y = [train_set_y[s] for s in sidx[:n_train]]
+    train_set_score = [train_set_score[s] for s in sidx[:n_train]]
 
-    train_set = (train_set_x, train_set_y)
-    valid_set = (valid_set_x, valid_set_y)
+    train_set = (train_set_x, train_set_y, train_set_score)
+    valid_set = (valid_set_x, valid_set_y, valid_set_score)
 
     def remove_unk(x):
         return [[1 if w >= n_words else w for w in sen] for sen in x]
 
-    test_set_x, test_set_y = test_set
-    valid_set_x, valid_set_y = valid_set
-    train_set_x, train_set_y = train_set
+    test_set_x, test_set_y, test_set_score = test_set
+    valid_set_x, valid_set_y, valid_set_score = valid_set
+    train_set_x, train_set_y, train_set_score = train_set
 
     train_set_x = remove_unk(train_set_x)
     valid_set_x = remove_unk(valid_set_x)
@@ -176,17 +182,24 @@ def load_data(path="imdb.pkl", n_words=100000, valid_portion=0.1, maxlen=None,
         sorted_index = len_argsort(test_set_x)
         test_set_x = [test_set_x[i] for i in sorted_index]
         test_set_y = [test_set_y[i] for i in sorted_index]
+        test_set_score = [test_set_score[i] for i in sorted_index]
 
         sorted_index = len_argsort(valid_set_x)
         valid_set_x = [valid_set_x[i] for i in sorted_index]
         valid_set_y = [valid_set_y[i] for i in sorted_index]
+        valid_set_score = [valid_set_score[i] for i in sorted_index]
 
         sorted_index = len_argsort(train_set_x)
         train_set_x = [train_set_x[i] for i in sorted_index]
         train_set_y = [train_set_y[i] for i in sorted_index]
+        train_set_score = [train_set_score[i] for i in sorted_index]
 
-    train = (train_set_x, train_set_y)
-    valid = (valid_set_x, valid_set_y)
-    test = (test_set_x, test_set_y)
+    train = (train_set_x, train_set_y, train_set_score)
+    valid = (valid_set_x, valid_set_y, valid_set_score)
+    test = (test_set_x, test_set_y, test_set_score)
 
-    return train, valid, test
+    pickle.dump(train, open('imdb.train.pkl', 'wb'), -1)
+    pickle.dump(valid, open('imdb.valid.pkl', 'wb'), -1)
+    pickle.dump(test, open('imdb.test.pkl', 'wb'), -1)
+
+    # return train, valid, test
